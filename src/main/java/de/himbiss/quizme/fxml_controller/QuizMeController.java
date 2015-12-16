076a234a7b22cb37ad22d22760a5aed74f1ad959
Vincent Ortland <vincent.ortland@gmx.de>
2015-12-16 20:56:39 +0100

    added hibernate with h2

---
+++ src/main/java/de/himbiss/quizme/fxml_controller/QuizMeController.java
@@ -0,0 +1,7 @@
+package de.himbiss.quizme.fxml_controller;
+
+/**
+ * Created by Vincent on 16.12.2015.
+ */
+public class QuizMeController {
+}
---
+++ src/main/java/de/himbiss/quizme/util/HibernateUtil.java
@@ -0,0 +1,7 @@
+package de.himbiss.quizme.util;
+
+/**
+ * Created by Vincent on 16.12.2015.
+ */
+public class HibernateUtil {
+}
